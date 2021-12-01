d2059b82ab3c734e5160b604e5ff1808aee29de3
KOH KAI QUAN <57383960+lusterless@users.noreply.github.com>
2021-12-02 01:15:46 +0800

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+set -x
+docker run -d -p 5000:5000 --name apptest --network testing theimg:latest
+sleep 1
+set +x
+
+echo 'Now...'
+echo 'Visit http://localhost to see your PHP application in action.'
+
